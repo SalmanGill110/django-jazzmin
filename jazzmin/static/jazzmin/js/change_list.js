0907c060cca998db8344b71f23995802331be56a
Salman Gill <56307949+SalmanGill110@users.noreply.github.com>
2020-12-23 16:20:16 +0200

    Update change_list.js

--- jazzmin/static/jazzmin/js/change_list.js
+++ jazzmin/static/jazzmin/js/change_list.js
@@ -24,7 +24,7 @@
 
         // Make search filters select2 and ensure they work for filtering
         const $ele = $('.search-filter');
-        // $ele.search_filters();
+        $ele.search_filters();
         //$ele.select2({ width: '100%' });
 
         // Use select2 for mptt dropdowns
